b97bcf2cd1b7df6fd0e854e9abc8df698564d469
Taya <31591090+tayadev@users.noreply.github.com>
2025-10-16 18:10:11 +0200

    Update fileSystems.nix

--- modules/hosts/workstation/fileSystems.nix
+++ modules/hosts/workstation/fileSystems.nix
@@ -4,5 +4,8 @@
       device = "/dev/disk/by-uuid/35d8178a-1e3c-4ef8-953f-4f94c1ee0f4f";
       fsType = "ext4";
     };
+
+fileSystems."/boot" =
+{device = "/dev/disk/by-uuid/BD9C-34BC"; fsType = "vfat"; options = [ "fmask=0022" "dmask=0022" ]; };
   };
 }
