7b173c2bf2df1b1a0e484b56ea65e5908f5b8012
Taya Crystals <taya@taya.net>
2025-10-16 19:06:20 +0000

    s

--- modules/hosts/workstation/boot.nix
+++ modules/hosts/workstation/boot.nix
@@ -1,9 +1,12 @@
 {
-  flake.modules.nixos."hosts/workstation".boot = {
-    loader = {
-      systemd-boot.enable = true;
-      efi.canTouchEfiVariables = true;
+  flake.modules.nixos."hosts/workstation".boot =
+    { pkgs }:
+    {
+      loader = {
+        systemd-boot.enable = true;
+        efi.canTouchEfiVariables = true;
+      };
+      kernelPackagess = pkgs.linuxPackages_latest;
+      kernelModules = [ "iwlwifi" ];
     };
-    kernelModules = [ "iwlwifi" ];
-  };
 }
