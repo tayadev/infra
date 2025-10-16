deaafe3faa57c3b236dc5d140e621d56fd8317d5
Taya Crystals <taya@taya.net>
2025-10-16 20:15:20 +0000

    s

--- modules/users/taya/default.nix
+++ modules/users/taya/default.nix
@@ -18,19 +18,20 @@
     homeManager.taya = {
       programs.mods = {
         enable = true;
-        settings = ''
-          {
-            default-model = "openai/gpt-4o-mini";
-            apis = {
-              openrouter = {
-                base-url = "https://openrouter.ai/api/v1";
-                models = {
-                "openai/gpt-4o-mini" = {max-input-chars=650000;}
-                }
-              }
-            }
-          }
-        '';
+        settings = {
+          default-model = "openai/gpt-4o-mini";
+          apis = {
+            openrouter = {
+              base-url = "https://openrouter.ai/api/v1";
+              api-key = "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"; # YES I KNOW THIS IS A BAD IDEA
+              models = {
+                "openai/gpt-4o-mini" = {
+                  max-input-chars = 650000;
+                };
+              };
+            };
+          };
+        };
       };
       programs.git = {
         enable = true;
